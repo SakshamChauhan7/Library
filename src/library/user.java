5fe9399658e3962d7af2ae0fc9e9c49ca4e4cd1b
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-08 14:41:26 -0500

    basic user attribute and get function

--- src/library/user.java
+++ src/library/user.java
@@ -1,1 +1,12 @@
 package library;
+
+
+public class user{
+	
+	//these are some basic attribute to user that will be 
+	//Utilized by other classes
+	
+	String name = null;
+	int userId;
+	int contactNum;
+}
