727b9cb9b3696c4a0f72a3e337b786b9efdc6d69
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-08 14:53:50 -0500

    adding all set method

--- src/library/user.java
+++ src/library/user.java
@@ -9,4 +9,27 @@ public class user{
 	String name = null;
 	int userId;
 	int contactNum;
+	
+	public user(String uName,int uId, int Cnum){
+		name = uName;
+		userId = uId;
+		contactNum = Cnum;
+		
+	}
+	
+	
+	
+	//All the get functions
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public int getContactNum() {
+		return contactNum;
+	}
 }
