31f4c49b4779a789a97d16afcdf11d051b49ca9a
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-08 14:27:56 -0500

    Adding my utility file

---
+++ src/library/myUtil.java
@@ -0,0 +1,24 @@
+package library;
+
+import java.util.Scanner;
+
+public class myUtil {
+
+	//Use "ask" when you print menu option so there is no new line for input
+		public static void ask(String message) {
+			System.out.print(message);
+		}
+		
+		//use "print" instead prinln command
+		public static void print(String message) {
+			System.out.println(message);
+		}
+		
+		// For new line
+		public static void print() {
+			System.out.println();
+		}
+		
+		
+		
+}
